6d0772bc8cf6f29cf5ba66ad320268337050940a
luthfizone <luthfizonedev@gmail.com>
2025-01-02 11:44:08 +0700

    feat: add function for open and close form add friend

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import FormAddFriend from "./components/form/FormAddFriend";
 import FormSplitBill from "./components/form/FormSplitBill";
 import FriendList from "./components/FriendList";
 import initialFriends from "./data/sample.json";
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  const handleShowAddFriend = () => {
+    setShowAddFriend((showAddFriend) => !showAddFriend);
+  };
+
   return (
     <div className="app-container">
       <div className="sidebar">
         <FriendList friends={initialFriends} />
-        <FormAddFriend />
-        <button className="button btn-add-friend">Add Friend</button>
+        {showAddFriend && <FormAddFriend />}
+        <button className="button btn-add-friend" onClick={handleShowAddFriend}>
+          {showAddFriend ? "Add Friend" : "Close"}
+        </button>
       </div>
       <FormSplitBill />
     </div>
