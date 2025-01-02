a43cececea2e5241d78b7a70bb6e382ba846346d
luthfizone <luthfizonedev@gmail.com>
2025-01-02 13:20:10 +0700

    feat: function for add new friend

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ import initialFriends from "./data/sample.json";
 
 function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
+  const [defaultFriend, setFriend] = useState(initialFriends);
 
   const handleShowAddFriend = () => {
     setShowAddFriend((showAddFriend) => !showAddFriend);
   };
 
+  const handleNewFriend = (friend) => {
+    setFriend([...defaultFriend, friend]);
+  };
+
   return (
     <div className="app-container">
       <div className="sidebar">
-        <FriendList friends={initialFriends} />
-        {showAddFriend && <FormAddFriend />}
+        <FriendList friends={defaultFriend} />
+        {showAddFriend && <FormAddFriend onHandleNewFriend={handleNewFriend} />}
       </div>
       <button className="button btn-add-friend" onClick={handleShowAddFriend}>
         {showAddFriend ? "Close" : "Add Friend"}
--- src/components/form/FormAddFriend.jsx
+++ src/components/form/FormAddFriend.jsx
@@ -1,19 +1,68 @@
-function FormAddFriend() {
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+function FormAddFriend({ onHandleNewFriend }) {
+  const [newFriend, setNewFriend] = useState("");
+  const [newImage, setNewImage] = useState("https://i.pravatar.cc/48");
+
+  const handleSubmitNewFriend = (event) => {
+    event.preventDefault();
+
+    if (!newFriend || !newImage) return;
+
+    // generate random id
+    const id = crypto.randomUUID();
+
+    // send this object
+    const newId = {
+      id: Number(id),
+      name: newFriend,
+      image: `${newImage}?=${id}`,
+      balance: 0,
+    };
+
+    // send the data
+    onHandleNewFriend(newId);
+
+    // empty the state after sending data
+    setNewFriend("");
+    setNewImage("");
+  };
+
   return (
-    <form className="form-add-friend">
+    // Form for handle the submit
+    <form className="form-add-friend" onSubmit={handleSubmitNewFriend}>
+      {/* name */}
       <label htmlFor="name" className="label-name">
         🤵🏻Name:{" "}
       </label>
-      <input type="text" name="name" id="name" className="input-name" />
+      <input
+        type="text"
+        name="name"
+        id="name"
+        className="input-name"
+        onChange={(event) => setNewFriend(event.target.value)}
+      />
 
+      {/* image */}
       <label htmlFor="image" className="label-image">
         📸Image:{" "}
       </label>
-      <input type="text" name="image" id="image" className="input-image" />
+      <input
+        type="text"
+        name="image"
+        id="image"
+        className="input-image"
+        onChange={(event) => setNewImage(event.target.value)}
+      />
 
       <button className="button">Add</button>
     </form>
   );
 }
+
+FormAddFriend.propTypes = {
+  onHandleNewFriend: PropTypes.func.isRequired,
+};
 
 export default FormAddFriend;
